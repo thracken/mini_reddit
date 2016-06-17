1673a9357eae8bf490975692c63c0a2f28db2937
Jeff <jeffgreenland.1@gmail.com>
2016-06-17 14:24:56 -0700

    Added comments, and all associations.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ActiveRecord::Base
+  validates :body, presence: true
+  validates :user_id, presence: true
+  validates :post_id, presence: true
+  belongs_to :user
+  belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   validates :link, presence: true
   validates :user_id, presence: true
   belongs_to :user
+  has_many :comments
 end
